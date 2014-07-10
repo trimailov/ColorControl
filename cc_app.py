fe39e83b9d73ef8d25a6d60e38f8cd6531042cda
Justas Trimailovas <j.trimailovas@gmail.com>
2014-07-11 00:39:35 +0300

    working on lamp connection

--- cc_app.py
+++ cc_app.py
@@ -1,9 +1,10 @@
 import wx
 import serial
+import time
 
 class AppFrame(wx.Frame):
     def __init__(self, title):
-        wx.Frame.__init__(self, parent=None, title=title, size=(400, 300))
+        wx.Frame.__init__(self, parent=None, title=title, size=(600, 400))
 
         filemenu = wx.Menu()
 
@@ -20,18 +21,21 @@ class AppFrame(wx.Frame):
 
 
 class SliderPanel(wx.Panel):
+    lamp = serial.Serial()
+
     def __init__(self, parent, id):
         wx.Panel.__init__(self, parent, id)
 
-        self.slider_red = wx.Slider(self, -1, 50, 0, 100, (50, 10), (300, 20),
+        self.slider_red = wx.Slider(self, -1, 5000, 0, 9999, (10, 10), (580, 20),
             wx.SL_HORIZONTAL)
-        self.slider_amber = wx.Slider(self, -1, 50, 0, 100, (50, 60), (300, 20),
+        self.slider_amber = wx.Slider(self, -1, 5000, 0, 9999, (10, 60), (580, 20),
             wx.SL_HORIZONTAL)
-        self.slider_green = wx.Slider(self, -1, 50, 0, 100, (50, 110), (300, 20),
+        self.slider_green = wx.Slider(self, -1, 5000, 0, 9999, (10, 110), (580, 20),
             wx.SL_HORIZONTAL)
-        self.slider_blue = wx.Slider(self, -1, 50, 0, 100, (50, 160), (300, 20),
+        self.slider_blue = wx.Slider(self, -1, 5000, 0, 9999, (10, 160), (580, 20),
             wx.SL_HORIZONTAL)
-        self.button = wx.Button(self, -1, label="open port", pos=(160, 200), size=(80, 40))
+        self.buttonConnect = wx.Button(self, -1, label="open port", pos=(10, 210))
+        self.buttonSend = wx.Button(self, -1, label="send value", pos=(10, 250))
 
         self.Bind(wx.EVT_SLIDER, self.sliderUpdate)
         self.Bind(wx.EVT_BUTTON, self.onButton)
@@ -47,10 +51,19 @@ class SliderPanel(wx.Panel):
                                                    self.pos_green, 
                                                    self.pos_blue)
 
+        send_string = ':0104 %i %i %i %i\r\n' % (self.pos_red, 
+                                                 self.pos_amber,
+                                                 self.pos_green, 
+                                                 self.pos_blue)
+        if self.lamp.isOpen():
+            self.lamp.write(send_string)
+            time.sleep(0.001)
+
         frame.SetTitle(str1)
 
     def onButton(self, event):
-        frame.SetTitle("Button was pressed")
+        self.lamp.port = '/dev/tty.LUME20R_130426-DevB'
+        self.lamp.timeout = 1
 
 app = wx.App(True)
 frame = AppFrame("ColorControl")
