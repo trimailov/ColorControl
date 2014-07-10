747fcb4a79b7f297bfb9cae3944330b0ab95f3ab
Justas Trimailovas <j.trimailovas@gmail.com>
2014-07-11 00:05:49 +0300

    Added menu bar with cmd+q functionality

--- cc_app.py
+++ cc_app.py
@@ -1,17 +1,38 @@
 import wx
+import serial
+
+class AppFrame(wx.Frame):
+    def __init__(self, title):
+        wx.Frame.__init__(self, parent=None, title=title, size=(400, 300))
+
+        filemenu = wx.Menu()
+
+        filemenu.Append(wx.ID_ABOUT, "&About", " Information about this program")
+        filemenu.AppendSeparator()
+        filemenu.Append(wx.ID_EXIT, "E&xit", " Terminate the program")
+
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu, "&File")
+        self.SetMenuBar(menuBar)
+
+    def on_close(self, event):
+        self.Destroy()
+
 
 class SliderPanel(wx.Panel):
     def __init__(self, parent, id):
         wx.Panel.__init__(self, parent, id)
 
-        self.slider_red = wx.Slider(self, -1, 50, 0, 100, (50, 50), (300, 20),
+        self.slider_red = wx.Slider(self, -1, 50, 0, 100, (50, 10), (300, 20),
             wx.SL_HORIZONTAL)
-        self.slider_amber = wx.Slider(self, -1, 50, 0, 100, (50, 100), (300, 20),
+        self.slider_amber = wx.Slider(self, -1, 50, 0, 100, (50, 60), (300, 20),
             wx.SL_HORIZONTAL)
-        self.slider_green = wx.Slider(self, -1, 50, 0, 100, (50, 150), (300, 20),
+        self.slider_green = wx.Slider(self, -1, 50, 0, 100, (50, 110), (300, 20),
             wx.SL_HORIZONTAL)
-        self.slider_blue = wx.Slider(self, -1, 50, 0, 100, (50, 200), (300, 20),
+        self.slider_blue = wx.Slider(self, -1, 50, 0, 100, (50, 160), (300, 20),
             wx.SL_HORIZONTAL)
+
+
 
         self.Bind(wx.EVT_SLIDER, self.slider_update)
 
@@ -28,9 +49,8 @@ class SliderPanel(wx.Panel):
 
         frame.SetTitle(str1)
 
-
 app = wx.App(True)
-frame = wx.Frame(None, -1, "Sliders", size=(400, 300))
+frame = AppFrame("ColorControl")
 SliderPanel(frame, -1)
 frame.Show(True)
 app.MainLoop()
